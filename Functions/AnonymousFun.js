a8a28e1af4d56646ad7c79c1a41f7374ca246f7a
Asmita Tagad <asmitatagad07@gmail.com>
2024-10-12 11:14:39 +0530

    Anonymous Function

---
+++ Functions/AnonymousFun.js
@@ -0,0 +1,9 @@
+//Anonymous Function: 
+// the function without name call anonymous function,and mostly used where function passed as arguments.
+
+setTimeout(function(){
+    console.log("Hello, Javascript!");
+},3000)
+
+
+//it is mostly used in the setTimeout or in the event handler functions
