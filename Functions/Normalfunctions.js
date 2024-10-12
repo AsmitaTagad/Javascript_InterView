d4fa4eaeb81662a52039ad7e4a36cf90ebbfa93a
Asmita Tagad <asmitatagad07@gmail.com>
2024-10-12 11:02:39 +0530

    normal function

---
+++ Functions/Normalfunctions.js
@@ -0,0 +1,7 @@
+//1. Function Declaration: it is normal function declared with function keyword and can be accessed anywhere (it is globally scoped and hoisted )
+
+// function greet(){
+//     console.log("Hello , My name is Asmita");
+
+// }
+// greet()
